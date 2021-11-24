b1babb240093b38bdc83c5b5014dfeb6a1432387
Rohit <53184437+Sai-Rohit@users.noreply.github.com>
2021-11-24 05:09:34 +0000

    updating gitignore

--- bin/main/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO servicelist VALUES (1,'GYM',10);
-INSERT INTO servicelist VALUES (2,'YOGA',10);
-INSERT INTO servicelist VALUES (3,'DOCTOR',30);
-INSERT INTO servicelist VALUES (4,'DIETITIAN',30);
-
-INSERT INTO employeedetails VALUES (1,'Anjali','DOCTOR','anjalids916@gmail.com');
-INSERT INTO employeedetails VALUES (2,'Anushka','YOGA','anushkachalla@gmail.com');
-INSERT INTO employeedetails VALUES (3,'Jack','GYM','jack42@gmail.com');
-INSERT INTO employeedetails VALUES (4,'Tom','DIETITIAN','tom97@gmail.com');
